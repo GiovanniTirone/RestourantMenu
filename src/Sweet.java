468d4f17f57b8b3d9528c26247c99cc6fc18450c
Pietro Gallina <pietrogallina96@gmail.com>
2022-10-16 21:48:43 +0200

    Add 3 params overload constructor methods for Sweet class

--- src/Sweet.java
+++ src/Sweet.java
@@ -1,2 +1,18 @@
 public class Sweet{
+
+    public Sweet(String name){
+        this.name = name;
+        this.ingredients = new ArrayList<>(Arrays.asList(name));
+    }
+
+    public Sweet(String name, String ingredient) {
+        this.name = name;
+        this.ingredients = new ArrayList<>(Arrays.asList(ingredient));
+    }
+
+    public Sweet(String name, List<String>ingredients) {
+        this.name = name;
+        this.ingredients = new ArrayList<>(ingredients);
+    }
+
 }
