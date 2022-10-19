34dd2e1196fb9a4e16ebba76cb0cfc8f42bc9473
GiovanniTirone <tironegiovanni4@gmail.com>
2022-10-19 10:11:58 +0200

    modify DishList class

--- src/it/restaurantMenu/menu/DishList.java
+++ src/it/restaurantMenu/menu/DishList.java
@@ -1,9 +1,43 @@
 package it.restaurantMenu.menu;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class DishList <T extends Dish> extends ArrayList <Dish> {
+    double pricesAverage;
 
+    public DishList () {
+        super();
+        this.pricesAverage = 0;
+    }
+
+    public void setPricesAverage () {
+        this.forEach(dish -> pricesAverage += dish.price);
+        pricesAverage /= this.size();
+    }
+
+    /*
+    public Set<Dish> getDishesWithAllergen (Allergen allergen ) {
+        Set<Dish> dishes = new HashSet<>();
+        for(Dish dish : this) {
+            if(dish.ingredients.ALLERGENS.contains(allergen)){
+                dishes.add(dish);
+            }
+        }
+        return dishes;
+    }
+    */
+    /*
+    public Set<Drink> getAlcoholicDrinks () {
+        if(!this.getClass().getName().equals("Drink")) return null;
+        Set<Drink> dishes = new HashSet<>();
+        for(Drink (Drink)drink : this) {
+
+        }
+    }
+    */
 
 
     @Override
